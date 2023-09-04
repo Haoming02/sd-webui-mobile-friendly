032722c1cf46c07a51680a386e83d268aaeae150
Haoming <hmstudy02@gmail.com>
2023-09-04 14:49:38 +0800

    beta
    Feedbacks are welcomed!

---
+++ README.md
@@ -0,0 +1,18 @@
+﻿# SD Webui Mobile Friendly 
+<h4 align="right"><i>BETA</i></h4>
+
+This is an Extension for the [Automatic1111 Webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui), 
+which changes the compositions of some UI to make it ~~slightly~~ easier to navigate when viewed in **portrait** mode.
+
+## Features
+1. Move the Generation Result under the **Generate** button
+    - **ie.** Above the parameter settings
+2. Add a new button at the bottom of the page, that scrolls to the top when clicked
+3. Make some buttons slightly larger
+    - Namely the `tool` class
+4. Increase the paddings between Sliders
+5. Center the buttons in a `form`
+
+<hr>
+
+<sup>This doesn't magically make the Webui run on mobile. This is meant for mobile usage enabled by the `--listen` launch argument.</sup>
---
+++ javascript/mobile.js
@@ -0,0 +1,35 @@
+﻿class OnTheGo {
+    static main() {
+        ['txt', 'img'].forEach((mode) => {
+            const it_btn = document.getElementById('interrogate')
+            it_btn.parentElement.style.display = 'contents'
+
+            const generate_btn = document.getElementById(mode + '2img_generate')
+
+            const box = generate_btn.parentElement
+            box.style.padding = '10px'
+
+            const result = document.getElementById(mode + '2img_results')
+            result.parentNode.querySelector('.resize-handle').remove()
+
+            const top_row = document.getElementById(mode + '2img_toprow')
+            top_row.append(result)
+
+            const jump = generate_btn.cloneNode()
+            jump.innerHTML = 'Jump To Top'
+
+            jump.addEventListener('click', () => window.scrollTo(0, 0))
+
+            jump.style.position = 'relative'
+            jump.style.minHeight = '2.5em'
+            jump.style.width = '80%'
+            jump.style.left = '10%'
+            jump.style.margin = '1em 0px 0px 0px'
+
+            const tab = document.getElementById('tab_' + mode + '2img')
+            tab.appendChild(jump)
+        })
+    }
+}
+
+onUiLoaded(async () => OnTheGo.main())
